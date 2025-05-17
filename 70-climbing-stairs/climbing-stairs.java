a51ed0c966792288bc067d3b2d37aeaa56725aad
devikam9 <108721278+devikam9@users.noreply.github.com>
2025-05-17 15:13:07 -0700

    Time: 0 ms (100.00%) | Memory: 40.3 MB (80.92%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,17 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
+        
         int[] res = new int[n + 1];
-        return distinctWays(n,res);    
-    }
-
-    public int distinctWays(int n, int[] res){
-        if(n == 0 || n == 1){
-            return 1;
+        res[0] = 1;
+        res[1] = 1;
+        for(int i = 2; i <= n; i++){
+            res[i] = res[i - 1] + res[i - 2];
         }
-        if(res[n] != 0){
-            return res[n];
-        }
-        res[n] = distinctWays(n - 1,res) + distinctWays(n - 2,res);
-        return res[n];
+        return res[n]; 
     }
 }
