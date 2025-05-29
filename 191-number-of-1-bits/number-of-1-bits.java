b3e4ea017dd9391f44569c6e11f8b9c959a9fb70
devikam9 <108721278+devikam9@users.noreply.github.com>
2025-05-29 15:00:51 -0700

    Time: 0 ms (100.00%) | Memory: 41.1 MB (12.59%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -13,22 +13,22 @@ class Solution {
         //1. while n > 0 - and every bit with 1
         // right shift n
         //1&1 = 1
-        // int res = 0;
-        // while(n > 0){
-        //     res += n & 1;
-        //     n = n >> 1;
-        // }
-        // return res;
+        int res = 0;
+        while(n > 0){
+            res += n & 1;
+            n = n >> 1;
+        }
+        return res;
 
         //approach 3
         //1. n > 0 : n & n - 1
         //2. Increment res every time.
 
-        int res = 0;
-        while(n > 0){
-            n = n & (n -1);
-            res++;
-        }
-        return res;
+        // int res = 0;
+        // while(n > 0){
+        //     n = n & (n -1);
+        //     res++;
+        // }
+        // return res;
     }
 }
