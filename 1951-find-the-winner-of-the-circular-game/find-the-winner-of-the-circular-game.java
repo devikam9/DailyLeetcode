c925b2f889818a60768e2c28768307ac3cda4d0f
devikam9 <108721278+devikam9@users.noreply.github.com>
2025-05-29 18:39:37 -0700

    Time: 3 ms (51.69%) | Memory: 41.2 MB (30.08%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        //1. keep a list and all the players
+        List<Integer> players = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            players.add(i);
+        }
+        //2. keep the currindex and a count to check if it reaches k
+        
+        int currIndex = 0;
+        //3. iterate the list till list size is equal to 1
+        while(players.size() > 1){            
+
+                // 4. use circular list to reset to the beginning index
+            int removeIndex = (currIndex + k -1) % players.size();
+                //5. remove them from list if they are at k position
+            players.remove(removeIndex);
+            currIndex = removeIndex;
+               
+            
+        }
+        //6. return the first element present in the list
+        return players.getFirst();
+
+    }
+}
