408fa112f012e55c8f6ac4cc0f7e82765019e95a
devikam9 <108721278+devikam9@users.noreply.github.com>
2025-05-29 19:05:29 -0700

    Time: 44 ms (12.43%) | Memory: 44.4 MB (16.27%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,26 +1,22 @@
 class Solution {
+
     public int findTheWinner(int n, int k) {
-        //1. keep a list and all the players
-        List<Integer> players = new ArrayList<>();
-        for(int i = 1; i <= n; i++){
-            players.add(i);
+        // Initialize queue with n friends
+        Queue<Integer> circle = new LinkedList<Integer>();
+        for (int i = 1; i <= n; i++) {
+            circle.add(i);
         }
-        //2. keep the currindex and a count to check if it reaches k
-        
-        int currIndex = 0;
-        //3. iterate the list till list size is equal to 1
-        while(players.size() > 1){            
 
-                // 4. use circular list to reset to the beginning index
-            int removeIndex = (currIndex + k -1) % players.size();
-                //5. remove them from list if they are at k position
-            players.remove(removeIndex);
-            currIndex = removeIndex;
-               
-            
+        // Perform eliminations while more than 1 player remains
+        while (circle.size() > 1) {
+            // Process the first k-1 friends without eliminating them
+            for (int i = 0; i < k - 1; i++) {
+                circle.add(circle.remove());
+            }
+            // Eliminate the k-th friend
+            circle.remove();
         }
-        //6. return the first element present in the list
-        return players.getFirst();
 
+        return circle.peek();
     }
 }
