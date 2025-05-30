ce6c7947f75f0a6c04aa959e1b8ff165909c95dd
devikam9 <108721278+devikam9@users.noreply.github.com>
2025-05-29 19:08:47 -0700

    Time: 43 ms (18.19%) | Memory: 44.4 MB (16.27%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,6 +1,8 @@
 class Solution {
 
     public int findTheWinner(int n, int k) {
+        //tc - adding elements - o(n) + add and remove ach element for k -1 times. we do this for n - 1 elements = (n - 1) * (k - 1) = O(n * k)
+        // sc - O(n)
         // Initialize queue with n friends
         Queue<Integer> circle = new LinkedList<Integer>();
         for (int i = 1; i <= n; i++) {
