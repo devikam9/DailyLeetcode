d2627b449308d87cef9fe7abf7ed192c9c34be42
devikam9 <108721278+devikam9@users.noreply.github.com>
2025-06-02 20:20:37 -0700

    Time: 0 ms (100.00%) | Memory: 44.7 MB (43.19%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -15,29 +15,39 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
+        //1. Insert in between nodes and increment temp = temp.next.next;
 
-        Map<Node,Node> copy = new HashMap<>();
         Node temp = head;
-
         while(temp != null){
-            copy.put(temp, new Node(temp.val));
-            temp = temp.next;
+            Node node = new Node(temp.val);
+            node.next = temp.next;
+            temp.next = node;
+            temp = temp.next.next;
         }
 
         temp = head;
+        //2. Now insert the random pointers
         while(temp != null){
-            Node next = temp.next;
-            Node rand = temp.random;
-            Node node = copy.get(temp);
-            node.next = copy.get(next);
-            if(rand != null){
-                node.random = copy.get(rand);
-            }else{
+            Node node = temp.next;
+            if(temp.random != null)
+                node.random = temp.random.next;
+            else
                 node.random = null;
-            }
+            temp = temp.next.next;
+        }
+
+        //3. insert the next nodes and return - so create a dummy node.
+        temp = head;
+        Node dummy = new Node(-1);
+        Node res = dummy;
+        while(temp != null){
+            res.next = temp.next;
+            res = res.next;
+            temp.next = temp.next.next;
             temp = temp.next;
             
         }
-        return copy.get(head);    
+    
+        return dummy.next;
     }
 }
