77490c9d146fefe918023085910a9fc49598f6f6
devikam9 <108721278+devikam9@users.noreply.github.com>
2025-06-05 15:00:58 -0700

    Time: 3 ms (50.83%) | Memory: 45.5 MB (92.56%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,26 +1,12 @@
 class Solution {
-    int[] memo;
     public boolean canJump(int[] nums) {
-        memo = new int[nums.length + 1];
-        Arrays.fill(memo,-1);
 
-        return helper(0,nums);    
-    }
-    private boolean helper(int index, int[] nums){
+        int max = 0;
 
-        if(index >= nums.length - 1) return true;
-
-        if(memo[index] != -1) return memo[index] == 1;
-        int val = nums[index];
-
-        for(int i = 1; i <= val; i++){
-            if(helper(index+i,nums) == true) {
-                memo[index] = 1;
-                return true;
-            } 
+        for(int i = 0; i < nums.length; i++){
+            if(i > max) return false;
+            max =Math.max(max, i + nums[i]);
         }
-        memo[index] = 0;
-        return false;
-        
+        return true;
     }
 }
