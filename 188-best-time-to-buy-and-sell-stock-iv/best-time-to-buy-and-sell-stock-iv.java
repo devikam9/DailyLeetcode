a01ee2a56c14d8c3bb6a48a6c614cf860e7a6ff6
devikam9 <108721278+devikam9@users.noreply.github.com>
2025-06-12 22:56:56 -0700

    Time: 8 ms (35.38%) | Memory: 44.7 MB (46.60%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,31 @@
+class Solution {
+    int profit;
+    int[][][] dp;
+    public int maxProfit(int k, int[] prices) {
+        int len = prices.length;
+        dp = new int[len][2][k+1];
+        for(int[][] row : dp){
+            for(int[] col : row){
+                Arrays.fill(col,-1);
+            }
+        }
+        return helper(prices,0,1,k);
+        
+    }
+
+    private int helper(int[] prices,int index, int buy, int capacity){
+
+        if(capacity <= 0 || index == prices.length) return 0;
+
+        if(dp[index][buy][capacity] != -1) return dp[index][buy][capacity];
+
+        if(buy == 1){
+            profit = Math.max(-prices[index] + helper(prices,index+1,0,capacity), 0 + helper(prices,index+1,1,capacity));
+        }else{
+            profit = Math.max(prices[index] + helper(prices,index+1,1,capacity-1), 0 + helper(prices,index+1,0,capacity));
+        }
+
+        return dp[index][buy][capacity] = profit;
+        
+    }
+}
