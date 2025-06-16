35c680e3970ac0be11d58a30676372277d165dee
devikam9 <108721278+devikam9@users.noreply.github.com>
2025-06-15 18:35:24 -0700

    Time: 1 ms (99.90%) | Memory: 41.8 MB (94.41%) - LeetSync

---
+++ 1542-consecutive-characters/consecutive-characters.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxPower(String s) {
+        //left = a[0], iterate the s and prev = current then increment max else prev = curr element
+
+        char left = s.charAt(0);
+        int max = 1, currMax = 1;
+
+        for(int i = 1; i < s.length(); i++){
+            if(left == s.charAt(i)){
+                currMax++;
+                max = Math.max(currMax,max);
+            }else{
+                currMax = 1;
+            }
+            left = s.charAt(i);
+            
+        }
+        return max;
+    }
+}
