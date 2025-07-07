cd3635a166d50d9fbb807d949a22ff8bc4bb8d9d
devikam9 <108721278+devikam9@users.noreply.github.com>
2025-07-06 17:41:56 -0700

    Time: 2 ms (68.18%) | Memory: 42.3 MB (86.09%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,42 +1,62 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        Queue<int[]> rotTom = new LinkedList<>();
-        int row = grid.length, col = grid[0].length, freshTomatoes = 0;
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
+        // you need to start exploding from all the 2's at a time - so BFS is best
+
+        //add all the fruits to queue and use level in queue
+
+        // if(grid[0][0] == 0) return 0;
+
+        Queue<Pair> que = new LinkedList<>();
+        int freshCount = 0, timeToRotten = -1;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == 2){
-                    rotTom.add(new int[]{i,j});
-                } else if(grid[i][j] == 1){
-                    freshTomatoes++;
+                    que.add(new Pair(i,j));
+                }else if(grid[i][j] == 1){
+                    freshCount++;
                 }
-                
             }
         }
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-        int minutes = -1;
-        while(!rotTom.isEmpty()){
-            int size = rotTom.size();
-            for(int i = 0; i < size; i++){
-                int[] values = rotTom.remove();
-                int ro = values[0];
-                int co = values[1];
 
-                for(int[] dir : dirs){
-                    int r = ro + dir[0];
-                    int c = co + dir[1];
+        int[] rows = new int[]{-1,+1,0,0};
+        int[] cols = new int[]{0,0,-1,+1};
 
-                    if( r >= 0 && c >= 0 && r < row && c < col){
-                        if(grid[r][c] == 1){
-                            grid[r][c] = 2;
-                            rotTom.add(new int[]{r,c});
-                            freshTomatoes--;
-                        }
+        while(!que.isEmpty()){
+            int level = que.size();
+
+            for(int j = 0; j < level; j++){
+                Pair ele = que.remove();
+                int row = ele.first;
+                int col = ele.second;
+
+
+                for(int i = 0; i < 4; i++){
+                int nRow = row + rows[i];
+                    int nCol = col + cols[i];
+
+                    if(nRow >= 0 && nRow < grid.length && nCol >= 0 && nCol < grid[0].length && grid[nRow][nCol] == 1){
+                        grid[nRow][nCol] = 2;
+                        que.add(new Pair(nRow,nCol));
+                        freshCount--;
                     }
-                }    
+                }
             }
-            minutes++;
+
+            timeToRotten++;  
         }
-        if(minutes == -1 && freshTomatoes == 0) return 0;
-        return freshTomatoes == 0 ? minutes : -1;    
+        if(timeToRotten == -1 && freshCount == 0) return 0;
+        return freshCount == 0 ? timeToRotten : -1;
+    }
+
+    
+}
+
+class Pair{
+    int first;
+    int second;
+
+    Pair(int first, int second){
+        this.first = first;
+        this.second = second;
     }
 }
