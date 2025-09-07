69f4a4ec3c430e92e238c758d16011d066d12254
devikam9 <108721278+devikam9@users.noreply.github.com>
2025-09-07 00:21:28 -0700

    Time: 5 ms (97.06%) | Memory: 46.2 MB (50.41%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.java
+++ 252-meeting-rooms/meeting-rooms.java
@@ -1,14 +1,16 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-
+        //1. sort based on start interval.
+        //2. check the next start interval < prev end index
         Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
-// the start of the next meeting is before the end of the current one, it means they overlap 
-        for(int i = 0; i < intervals.length - 1; i++){    
-                if(intervals[i+1][0] < intervals[i][1]){
-                    return false;
-                }    
-        }
-        return true;
+
         
+        for(int i = 1; i < intervals.length; i++){
+            if(intervals[i][0] < intervals[i-1][1]){
+                return false;
+            }  
+        }
+
+        return true;
     }
 }
